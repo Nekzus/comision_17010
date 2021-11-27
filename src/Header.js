5e5e27fabe9ca1f58fba0cdbad52a86b02523095
Horacio Gutierrez <horacio.estevez@gmail.com>
2021-11-27 10:38:39 -0300

    After 24/11

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,13 @@
+import CartWidget from "./CartWidget";
 import Nav from "./Nav"
 
 const Header = ({producto, saludo}) => {
     
-    //const {producto, saludo} = parametros
-
-    console.log(producto)
-    //const otroTitulo = parametros.producto.titulo + " Mas texto"
-
-    //los props son de solo lectura , no deberia cambiarles el valor
-    //parametros.producto.titulo = "Producto 2"
-
-    //console.log(otroTitulo)
-
     return (
         <header id="main-header">
             <h1>{saludo}</h1>
             <Nav />
+            <CartWidget/>
         </header>
     )
 }
