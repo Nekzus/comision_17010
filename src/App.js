aad0d538e9c39dfae85053d8e61dca98ded5e5e3
Horacio Gutierrez <horacio.estevez@gmail.com>
2021-11-27 17:23:52 -0300

    Clase 3 promesas + efectos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import {useState} from "react"
+import { useState, useEffect } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import Main from "./Main"
 
+const productosIniciales = [
+    { id : 1, nombre : "Producto 1" },
+    { id : 2, nombre : "Producto 2" },
+    { id : 3, nombre : "Producto 3" },
+    { id : 4, nombre : "Producto 4" }
+]
 
 
 const App = () => {
-    
-    
-    const producto = {titulo:"Producto 1"}
+
+
+    const producto = { titulo: "Producto 1" }
     const saludo = "Hola Mundo"
-    
+
     //let contador = 0
     //const resultado = useState(0)
     //const contador = resultado[0]
@@ -19,8 +25,42 @@ const App = () => {
     const contadorInicial = 0
 
     const [contador, setContador] = useState(contadorInicial)
-    
-    
+    const [mensaje, setMensaje] = useState("Todavia no hay nada, cargando...")
+    const [productos,setProductos] = useState([])
+
+    useEffect(() => {
+
+        const promesa = new Promise((res, rej) => {
+
+            setTimeout(() => {
+                //console.log("Aca pido un array de productos a una DB")
+                if (Math.random() > 0.5) {
+                    res(productosIniciales)
+                }else{
+                    rej()
+                }
+            }, 2000)
+
+        })
+        //console.log(promesa)
+        //promesa.then(()=>{})
+        //promesa.catch(()=>{})
+        promesa
+            .then((resultado) => {
+                //console.log("Salio todo bien")
+                setMensaje("Todo salio bien")
+                setProductos(resultado)
+            })
+            .catch(() => {
+                //console.log("Salio todo mal")
+                setMensaje("Todo salio Mal")
+            })
+
+        //promesa.finally()
+
+
+    }, [])
+
     const aumentarContador = () => {
         console.log("Aumentar contador")
         //contador++
@@ -29,17 +69,19 @@ const App = () => {
         //console.log(contador)
     }
 
+
     return (
         <>
             <Header producto={producto} saludo={saludo} />
-            <Main>
+            <Main productos={productos}>
                 <h1>Test Props</h1>
+                <p>{mensaje}</p>
                 <p>El contador va : {contador}</p>
                 <button onClick={aumentarContador} >aumentar</button>
             </Main>
-            <Footer saludo={saludo}/>
+            <Footer saludo={saludo} />
         </>
-       
+
     )
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,17 @@
 import Button from 'react-bootstrap/Button'
 import ItemListContainer from './ItemListContainer'
 
-const Main = (props) => {
+const Main = ({productos,children}) => {
 
     return (
         <main>
             <h2>Home</h2>
-            {props.children}
+            {children}
+            
+            {productos.map((elemento,indice)=>{
+                return <p>{elemento.nombre}</p>
+            })}
+
             <ItemListContainer greeting="Bienvenidos al E-Commerce!"/>
             <Button variant="primary">Primary</Button>
         </main>
